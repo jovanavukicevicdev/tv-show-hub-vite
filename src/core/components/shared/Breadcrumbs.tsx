f0f95c5bf35fa662a080ff80841f4a1c94393e6f
Jovana <jovanav675@gmail.com>
2023-10-23 21:34:24 +0200

    Fix breadcrumbs layout issues

--- src/core/components/shared/Breadcrumbs.tsx
+++ src/core/components/shared/Breadcrumbs.tsx
@@ -1,32 +1,41 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Breadcrumbs as MuiBreadcrumbs } from '@mui/material';
 import styled from '@emotion/styled';
+import { getVar } from '../../theme/ui-variables/ui-variables.ts';
 
 const Breadcrumbs = () => {
   const location = useLocation();
   const crumbs = location.pathname.split('/').filter((crumb) => crumb !== '');
 
   return (
-    <StyledBreadcrumbs separator="›">
-      {crumbs.map((crumb, index) => {
-        const last = index === crumbs.length - 1;
-        const to = `/${crumbs.slice(0, index + 1).join('/')}`;
+    <BreadcrumbsContainer>
+      <StyledBreadcrumbs separator="›">
+        {crumbs.map((crumb, index) => {
+          const last = index === crumbs.length - 1;
+          const to = `/${crumbs.slice(0, index + 1).join('/')}`;
 
-        return (
-          <div key={crumb}>
-            {crumbs.length === 1 && crumb === 'shows' ? null : last ? (
-              <span>{crumb}</span>
-            ) : (
-              <Link to={to}>{crumb}</Link>
-            )}
-          </div>
-        );
-      })}
-    </StyledBreadcrumbs>
+          return (
+            <div key={crumb}>
+              {crumbs.length === 1 && crumb === 'shows' ? null : last ? (
+                <span>{crumb}</span>
+              ) : (
+                <Link to={to}>{crumb}</Link>
+              )}
+            </div>
+          );
+        })}
+      </StyledBreadcrumbs>
+    </BreadcrumbsContainer>
   );
 };
 
 export default Breadcrumbs;
+
+const BreadcrumbsContainer = styled.div`
+  width: 100%;
+  max-width: ${getVar('contentMaxWidth')};
+  margin: 0 auto;
+`;
 
 const StyledBreadcrumbs = styled(MuiBreadcrumbs)`
   font-size: 14px;
